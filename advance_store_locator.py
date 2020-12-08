adf68c1408fd26cdbe8de544d88452eb18865555
MAYUR <mayurbiw@gmail.com>
2020-12-08 13:42:29 +0530

    Solving parrallel request isssue

--- advance_store_locator.py
+++ advance_store_locator.py
@@ -27,8 +27,8 @@ all_stores_us = dict()
 # fetch  all zip codes from the excel file and store into a variable and create all urls 
 
 for i in range(1,sheet.nrows):
-    if i == 4000:
-        break
+    #if i == 5000:
+     #   break
     z = sheet.cell_value(i, 0)
     if z == '':
         break
@@ -40,85 +40,83 @@ for i in range(1,sheet.nrows):
 
 
 print("Number of urls to hit "  + str(len(urls)))
-
-MAX_CONNECTIONS = 1000
+all_stores_us_id = []
+MAX_CONNECTIONS = 200
 resposes = []
 pages= 0 
-#for x in range(0,len(urls), MAX_CONNECTIONS):
- #   print(f"URLS range {x} to {x + MAX_CONNECTIONS}" )
-  #  rs = (grequests.get(u, stream=False) for u in urls[x:x+MAX_CONNECTIONS])
-   # time.sleep(0.2) #You can change this to whatever you see works better. 
-    #resposes.extend(grequests.map(rs)) #The key here is to extend, not append, not insert. 
-    #print("Waiting") #Optional, so you see something is done. 
+for x in range(0,len(urls), MAX_CONNECTIONS):
+    #print(f"URLS range {x} to {x + MAX_CONNECTIONS}" )
+    rs = (grequests.get(u, stream=False) for u in urls[x:x+MAX_CONNECTIONS])
+    
+    # making all the reqest at once. 
+    resposes = grequests.map(rs)
 
+    # for each respond finding the store. 
+    for res in resposes:
 
-
-rs = (grequests.get(u,stream=False) for u in urls)
-resposes = grequests.map(rs)
-
-
-
-all_stores_us_id = []
-for res in resposes:
-
-    #print("in for...................")
-
-    if res is None:
-        continue
-
-    if res is not None and res.status_code == 200:
-        soup = BeautifulSoup(res.content, 'html.parser')
-        all_script_tags = list(soup.select("body script"))
-
-        # get the script tag where json object corresponding to all 100 stores is located. 
-        stores =  (str((all_script_tags[3])))
-
-        #print(store_details)
-
-        # get the json that contains store details.
-        store_details = re.findall("(?s)(?<=window.__BOOTSTRAP = )(.*?)(?=window.__INTL_MESSAGES)", stores)
-
-
-        # find all retuns the list of matches, above it will always return one recoed. 
-        # converting json into dictionary.
-        dict = json.loads(str(store_details[0]))
-
-        # finding number of records returned by the api call 
-        #returned = dict['previousAction']['payload']['data']['paging']['returned']
-
-        check_zero = dict.get('previousAction').get('payload')
-
-        if check_zero is None:
-            returned = None
-        else:
-            returned = dict['previousAction']['payload']['data']['paging']['returned']
-
-        if returned is None:
+        if res is None:
             continue
 
-        for i in range(0,returned):
+        if res is not None and res.status_code == 200:
+            soup = BeautifulSoup(res.content, 'html.parser')
+            all_script_tags = list(soup.select("body script"))
 
-            store = dict['previousAction']['payload']['data']['stores'][i]
-            
-            s = {
-                'store_number': store['storeNumber'] ,
-                'name':  store['name'],
-                'latitude': store['coordinates']['latitude']  ,
-                'longitude': store['coordinates']['longitude'] ,
-                'city': store['address']['city'],
-                'state': store['address']['countrySubdivisionCode'],
-                'phone_number': store['phoneNumber']
-                #'zipcode': zipcode
-            }
-            
-            all_stores_us[store['id']] = s
-            
-            if store['id'] not in all_stores_us_id:
-                all_stores_us_id.append(store['id'])
-            
+            # get the script tag where json object corresponding to all 100 stores is located. 
+            stores =  (str((all_script_tags[3])))
+
+            #print(store_details)
+
+            # get the json that contains store details.
+            store_details = re.findall("(?s)(?<=window.__BOOTSTRAP = )(.*?)(?=window.__INTL_MESSAGES)", stores)
+
+
+            # find all retuns the list of matches, above it will always return one recoed. 
+            # converting json into dictionary.
+            dict = json.loads(str(store_details[0]))
+
+            # finding number of records returned by the api call 
+            #returned = dict['previousAction']['payload']['data']['paging']['returned']
+
+            check_zero = dict.get('previousAction').get('payload')
+
+            if check_zero is None:
+                returned = None
+            else:
+                returned = dict['previousAction']['payload']['data']['paging']['returned']
+
+            if returned is None:
+                continue
+
+            for i in range(0,returned):
+
+                store = dict['previousAction']['payload']['data']['stores'][i]
+                
+                s = {
+                    'store_number': store['storeNumber'] ,
+                    'name':  store['name'],
+                    'latitude': store['coordinates']['latitude']  ,
+                    'longitude': store['coordinates']['longitude'] ,
+                    'city': store['address']['city'],
+                    'state': store['address']['countrySubdivisionCode'],
+                    'phone_number': store['phoneNumber']
+                    #'zipcode': zipcode
+                }
+                
+                all_stores_us[store['id']] = s
+
+                #print(store['id'])
+                
+                if store['id'] not in all_stores_us_id:
+                    all_stores_us_id.append(store['id'])
+                
             res.close()
+        
+     
+    resposes = None 
+    time.sleep(1) #You can change this to whatever you see works better.
     
 
+    
 #print(all_stores_us)
 print("Uniques stores found "+str(len(all_stores_us_id)))
 
